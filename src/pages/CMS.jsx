79dbb50e15d7692bf2bc601fdc54ae1b821f94df
Penny <penny.wu@incrte.com>
2022-08-18 15:54:31 +0800

    useState布林值型別修正

--- src/pages/CMS.jsx
+++ src/pages/CMS.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import { Form, Container, Button } from "react-bootstrap";
 
 function CMS(props) {
   const [id, setId] = useState();
-  const [isListCgange, setIsListCgange] = useState("false");
-  const [isEdit, setIsEdit] = useState("false");
+  const [isListChange, setIsListChange] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
   const [list, setList] = useState([]);
   const [name, setName] = useState("");
   const [age, setAge] = useState();
@@ -17,11 +17,12 @@ function CMS(props) {
       .then((res) => res.json())
       .then((data) => {
         setList(data);
+        console.log(`default => ${isListChange}`);
       });
-  }, [name,isListCgange]);
+  }, [isListChange]);
 
   function add() {
-    if(name !== ""){
+    if (name !== "") {
       fetch(`http://localhost:3001/employee/`, {
         method: "POST",
         headers: {
@@ -35,24 +36,30 @@ function CMS(props) {
           password: `${password}`,
         }),
       })
-      .then(setIsListCgange(!isListCgange),setName(''),setAge(''),setPassword(''))
-    }else{
-    alert('請輸入內容')
+        .then(
+          setIsListChange(!isListChange),
+          setName(""),
+          setAge(""),
+          setPassword("")
+        )
+        .then(console.log(`add => ${isListChange}`));
+    } else {
+      alert("請輸入內容");
+    }
   }
-  }
-    
-    const toEdit=(e)=>{
+
+  const toEdit = (e) => {
     let id = e.target.parentNode.parentNode.firstChild.innerText;
-    let editObj = list.filter(x=>x.id == id)[0]
-    setIsEdit("true")
-    setId(id)
-    setName(editObj.name)
-    setAge(editObj.age)
-    setPassword(editObj.password)
-    console.log(isEdit);
-  }
-const edit=(e)=>{
-    fetch(`http://localhost:3001/employee/${id}`,{
+    let editObj = list.filter((x) => x.id === id * 1)[0];
+    setIsEdit(true);
+    setId(id);
+    setName(editObj.name);
+    setAge(editObj.age);
+    setPassword(editObj.password);
+    console.log(`編輯中嗎？${isEdit}`);
+  };
+  const edit = (e) => {
+    fetch(`http://localhost:3001/employee/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
@@ -62,28 +69,36 @@ const edit=(e)=>{
         id: `${id}`,
         name: `${name}`,
         age: `${age}`,
-        password: `${password}`
+        password: `${password}`,
       }),
-    }).then(setIsListCgange(!isListCgange),setName(''),setAge(''),setPassword('')).then(alert("修改成功！"))
-  }
-const cancelEdit = ()=>{
-  setIsEdit("false")
-  setName('')
-  setAge('')
-  setPassword('');
-}
-  
+    })
+      .then(
+        setIsListChange(!isListChange),
+        setName(""),
+        setAge(""),
+        setPassword("")
+      )
+      .then(alert("修改成功！"));
+  };
+  const cancelEdit = () => {
+    setIsEdit(false);
+    setName("");
+    setAge("");
+    setPassword("");
+  };
 
   const listItem = list.map((v, i) => (
     <tbody key={i}>
       <tr>
-        <td >{list[i].id}</td>
+        <td>{list[i].id}</td>
         <td>{list[i].name}</td>
         <td>{list[i].age}</td>
         {/* <td></td>
         <td></td> */}
         <td className="project-actions text-right">
-          <Button className="btn btn-primary btn-sm " onClick={toEdit}>修改</Button>
+          <Button className="btn btn-primary btn-sm " onClick={toEdit}>
+            修改
+          </Button>
           <Button
             className="btn btn-danger btn-sm"
             onClick={() =>
@@ -94,7 +109,7 @@ const cancelEdit = ()=>{
                   Accept: "application/json",
                 },
                 body: JSON.stringify({ id: `${list[i].id}` }),
-              }).then(setIsListCgange(!isListCgange))
+              }).then(setIsListChange(!isListChange))
             }
           >
             刪除
@@ -111,9 +126,11 @@ const cancelEdit = ()=>{
           <div className="container-fluid">
             <div className="row mb-2">
               <div className="col-sm-6">
-     <NavLink className="text-decoration-none" to="/">回前台</NavLink>
+                <NavLink className="text-decoration-none" to="/">
+                  回前台
+                </NavLink>
                 <h3>員工列表</h3>
-            <NavLink to="/work">出勤紀錄</NavLink>
+                <NavLink to="/work">出勤紀錄</NavLink>
               </div>
               <Form className="col-sm-6">
                 <Form.Control
@@ -139,21 +156,23 @@ const cancelEdit = ()=>{
                   aria-describedby="basic-addon1"
                   onChange={(e) => setPassword(e.target.value)}
                 />
-         {(isEdit !== 'false')?
-         <>
-                <Button className={`btn btn-danger btn-bg`} onClick={edit}>
-                  修改
-                </Button>
-                <Button className={`btn btn-secondary btn-bg `} onClick={cancelEdit}>
-                  取消修改
-                </Button>
-         </>
-                  :
+                {isEdit !== false ? (
+                  <>
+                    <Button className={`btn btn-danger btn-bg`} onClick={edit}>
+                      修改
+                    </Button>
+                    <Button
+                      className={`btn btn-secondary btn-bg `}
+                      onClick={cancelEdit}
+                    >
+                      取消修改
+                    </Button>
+                  </>
+                ) : (
                   <Button className={`btn btn-warning btn-bg`} onClick={add}>
-                  新增
-                </Button>
-                }
-               
+                    新增
+                  </Button>
+                )}
               </Form>
             </div>
           </div>
